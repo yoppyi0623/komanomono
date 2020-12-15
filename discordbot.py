143f3a088f4b0dce7fca32fdcad99685911b7bb1
yoppyi0623 <59519214+yoppyi0623@users.noreply.github.com>
2020-12-15 22:04:58 +0900

    implement new2

--- discordbot.py
+++ discordbot.py
@@ -17,5 +17,8 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
-
+@bot.command()
+    async def uso(ctx):
+        await ctx.send('なんだろう嘘つくのやめてもらっていいですか')
+        
 bot.run(token)
